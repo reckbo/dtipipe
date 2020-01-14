e64452d0a40948e0a19b21b991ed76cdfa6ecd1c
Ryan Eckbo <ryan.eckbo@mgh.harvard.edu>
2020-01-14 11:18:51 -0500

    STY: rename

--- src/dtipipe/pipeline/standard_pnl/UkfTracts.py
+++ src/dtipipe/pipeline/standard_pnl/UkfTracts.py
@@ -5,7 +5,6 @@ from plumbum import local
 
 from dtipipe import ukf_tract_querier
 from dtipipe import REPO_DIR
-from dtipipe import util
 from .BaseTask import BaseTask
 from .WmparcInDwi import WmparcInDwi
 from .Ukf import Ukf
@@ -22,19 +21,19 @@ class UkfTracts(BaseTask):
     num_proc_ukf_tract_querier = IntParameter()
 
     def requires(self):
-        return dict(tractography=self.clone(Ukf),
-                    atlas=self.clone(WmparcInDwi))
+        return dict(ukf=self.clone(Ukf),
+                    wmparc_in_dwi=self.clone(WmparcInDwi))
 
     def output(self):
         stem = local.path(self.tract_querier_query_file).stem
         return local.path(self.output_session_dir) / f'{self.output_basename}.tracts.{stem}'
 
     def run(self):
-        ukf_tract_querier.ukf_tract_querier(ukf_vtk=self.input()['tractography'],
-                                            atlas_file=self.input()['atlas']['wmparc_in_dwi'],
+        ukf_tract_querier.ukf_tract_querier(ukf_vtk=self.input()['ukf'],
+                                            atlas_file=self.input()['wmparc_in_dwi']['wmparc_in_dwi'],
                                             query_file=self.tract_querier_query_file,
                                             output_dir=self.output(),
                                             num_proc=self.num_proc_ukf_tract_querier)
 
     def freeview_wmparc_in_dwi(self):
-        self.requires()['atlas'].freeview()
+        self.requires()['wmparc_in_dwi'].freeview()
